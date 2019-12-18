6a391af35477f713e8335be4edbb8365c45affd2
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-12-18 11:59:59 +0200

    Adding env variables to config v4

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}/room-gen?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(conn => {
 // const db = mongoose.connect('mongodb+srv://root:good_grades123@good-grades-ou0xu.mongodb.net/room-gen?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(conn => {
+    console.log(`DB user: ${process.env.MONGO_USER}`);
     if (conn)
         console.log('Connected to Database');
 }).catch(error => {
