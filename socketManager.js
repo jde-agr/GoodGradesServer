7ba4a2e2f299a9900132fe13696c136465f408f4
Xeno RHODA <xeno@zarobi.co.za>
2020-01-27 08:19:13 +0200

    Socket Server Cahnges

--- socketManager.js
+++ socketManager.js
@@ -16,39 +16,34 @@ module.exports = function(socket){
 	})
 
 	socket.on(USER_CONNECTED, (user)=>{
-		console.log('This user has connected: ')
-		console.log(user);
+		console.log('User Connected:    ', user.givenName, user.unique_id);
 		user.socketId = socket.id;
 		connectedUsers = addUser(connectedUsers, user)
 		socket.user = user
-
-
-		// socketIO.io.emit(USER_CONNECTED, connectedUsers)
-		console.log("Connected Users: " + JSON.stringify(connectedUsers));
+		socketIO.io.emit(USER_CONNECTED, connectedUsers)
+		// console.log("Connected Users: " + JSON.stringify(connectedUsers));
 	})
 
 	socket.on('disconnect', ()=>{
 		if("user" in socket){
-			console.log("Socket user:")
-			console.log(socket.user)
-			connectedUsers = removeUser(connectedUsers, socket.user.name)
-
+			connectedUsers = removeUser(connectedUsers, socket.user.unique_id)
 			socketIO.io.emit(USER_DISCONNECTED, connectedUsers)
-			console.log("Disconnect", connectedUsers);
+			displayConnectedUsers()
 		}
 	})
 
 	//User logsout
 	socket.on(LOGOUT, ()=>{
-		connectedUsers = removeUser(connectedUsers, socket.user.name)
+		connectedUsers = removeUser(connectedUsers, socket.user.unique_id)
 		socketIO.io.emit(USER_DISCONNECTED, connectedUsers)
-		console.log("Disconnect", connectedUsers);
+		console.log("User Disconnected: ", socket.user.givenName, socket.user.unique_id);
+		displayConnectedUsers();
 	})
 
 	socket.on(NOTIFICATION, (message, receiver)=>{
 		console.log(receiver)
 		console.log('Notification received and sending it to', receiver.user_name)
-		if (isUser(connectedUsers, receiver.user_name) ){
+		if (isUser(connectedUsers, receiver.unique_id) ){
 			console.log("USER IS CONNECTED");
 			const receiverSocket = connectedUsers[receiver.user_name].socketId
 			socket.to(receiverSocket).emit(NOTIFICATION, message)
@@ -64,12 +59,24 @@ function addUser(userList, user){
 	return newList
 }
 
-function removeUser(userList, username){
+function removeUser(userList, unique_id){
 	let newList = Object.assign({}, userList)
-	delete newList[username]
+	delete newList[unique_id]
 	return newList
 }
 
-function isUser(userList, username){
-	return username in userList
+function isUser(userList, unique_id){
+	return unique_id in userList
+}
+
+function displayConnectedUsers(){
+	if (Object.keys(connectedUsers).length > 0){
+		console.log("Connected Users:");
+		for (let elm in connectedUsers) {
+			console.log("-", elm.user.givenName, elm.user.unique_id);
+		}
+	}
+	else{
+		console.log("------No users are connected------")
+	}
 }
